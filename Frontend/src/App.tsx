33dcf0b3159eddb22a75e725b3f335a1b56b8748
Suphakon <suphakon_ch@cmu.ac.th>
2026-01-26 22:03:22 +0700

    จำลองการแสดงผล Datacard3

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { WaterLevelChart } from './component/WaterLevelChart';
 import{StatusCard} from './component/StatusCard';
-import{DataCard} from './component/DataCard';
+import DataCard from './component/DataCard';
 function App() {
   return (
     <div className="container" style={{ marginTop: '40px', paddingBottom: '40px' }}>
@@ -15,8 +15,32 @@ function App() {
           ระบบบริหารจัดการน้ำและแจ้งเตือนภัยพิบัติอัจฉริยะ อบจ.ลำพูน
         </p>
       </div>
-      <div>
-        <DataCard/>
+      <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', marginBottom: '40px' }}>
+        
+        {/* ใบที่ 1 */}
+        <DataCard 
+            title="จำนวนสถานี" 
+            value={1} 
+            unit="สถานี" 
+            theme="orange" 
+        />
+
+        {/* ใบที่ 2 */}
+        <DataCard 
+            title="ระดับน้ำ" 
+            value="150.250" 
+            unit="เมตร" 
+            theme="blue" 
+        />
+
+        {/* ใบที่ 3 */}
+        <DataCard 
+            title="ปริมาณน้ำฝนสะสม" 
+            value="50.568" 
+            unit="มิลลิเมตร/ชม." 
+            theme="blue" 
+        />
+        
       </div>
     <div>
       {/*<StatusCard />*/}
