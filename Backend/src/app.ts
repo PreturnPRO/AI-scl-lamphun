185e33a881eeed80882378e983d29ada63c51ade
Nonnipat Tangrojjanakhajorn <pinozen.thailand@gmail.com>
2026-01-26 22:32:43 +0700

    fix error

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { Database, DeviceInfo, SensorData } from './database';
+import { Database, SensorData } from './database';
 import dotenv from 'dotenv';
 import axios from 'axios';
 
